7edfd5165b53157fd873ac314a81bc6b33c62c9f
Giiiiiiin <malisamiguel05@gmail.com>
2025-03-21 21:49:08 +0800

    Version 1.0

--- midterm-project-react-native/App.tsx
+++ midterm-project-react-native/App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import AppNavigator from './src/navigation/AppNavigator';
+import { styles } from './src/styles/globalStyles';
+import { GlobalProvider } from './src/context/globalContext';
+import FlashMessage from "react-native-flash-message";
+import Header from './src/components/Header';
+import Footer from './src/components/Footer';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <GlobalProvider>
+          <AppNavigator />
+          <Footer />
+        </GlobalProvider>
+      </SafeAreaView>
+      <FlashMessage position="bottom" />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
